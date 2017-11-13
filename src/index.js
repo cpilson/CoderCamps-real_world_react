9781298ba192a602344d75dba2dd4a845e5502fc
Chris Pilson <Chris.Pilson@gmail.com>
2017-11-13 11:44:44 -0700

    Initial Scaffolding

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import React from "react";
+import App from "./App";
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
-}
+const defaultState = {
+  appName: "Meowdium",
+  artciles: null
+};
 
-ReactDOM.render((
-  <App />
-), document.getElementById('root'));
+const reducer = function(state = defaultState, action) {
+  return state;
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
