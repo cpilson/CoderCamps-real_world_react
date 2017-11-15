8d145439cc9890c104d416490ba1befbd84eec04
Chris Pilson <Chris.Pilson@gmail.com>
2017-11-15 13:23:30 -0700

    Fixed header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,9 @@ class Header extends Component {
       <div>
         <nav className="nav navbar-light">
           <div className="container">
+            <Link to="/" className="navbar-brand">
+              {this.props.appName.toLowerCase()}
+            </Link>
             <ul className="nav navbar-nav pull-xs-right">
               <li className="nav-item">
                 <Link to="/" className="nav-link">
